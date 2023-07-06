4ebaa9c30e99384b80e49be8f7dcb584f28d15e4
Richard Chalk <barmybrit@gmail.com>
2023-07-06 13:36:51 +0200

    Add project files.

---
+++ DelegatesDemo/MathDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    public delegate int MathDelegate(int a, int b);
+    
+    // Vi behöver ingen class här!
+    //internal class MathDelegate
+    //{
+    //}
+}
---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DelegatesDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MathDelegate mathOperation = Add;
+            int a = 5;
+            int b = 7;
+            int result = mathOperation(a, b);
+
+            Console.WriteLine(result);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Power(int baseNumber, int exponent)
+        {
+            return (int)Math.Pow(baseNumber, exponent);
+        }
+    }
+}
